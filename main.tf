947ae43a8fa05d6873591a49319fbfd096218429
Colin <53243129+cfergs@users.noreply.github.com>
2019-08-03 17:58:37 +1200

    lambda bucket perms

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true #Forcably delete the bucket & contents if i run terraform destroy
 }
 
-resource "aws_lambda_function" "lambda" {
+resource "aws_lambda_function" "transactionprocessor" {
   function_name = "TransactionProcessor"
   description   = "Process data and send to DynamoDB tables"
   runtime       = "python2.7"
@@ -23,15 +23,6 @@ resource "aws_lambda_function" "lambda" {
   filename      = "TransactionProcessor.zip"
   handler       = "lambda_function.lambda_handler"
   timeout       = "20"
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${aws_s3_bucket.bucket.id}"
-
-  lambda_function {
-    lambda_function_arn = "${aws_lambda_function.lambda.arn}"
-    events              = ["s3:ObjectCreated:*"]
-  }
 }
 
 resource "aws_lambda_function" "totalnotifier" {
@@ -44,7 +35,26 @@ resource "aws_lambda_function" "totalnotifier" {
   timeout       = "20"
 }
 
-resource "aws_lambda_event_source_mapping" "example" {
+#lambda to grant access to S3
+resource "aws_lambda_permission" "lambdaS3_permission" {
+  depends_on    = ["aws_lambda_function.transactionprocessor"]
+  statement_id  = "AllowWildrydesAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.transactionprocessor.function_name}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.bucket.arn}"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket      = "${aws_s3_bucket.bucket.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.transactionprocessor.arn}"
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "dynamodb_transactions" {
   event_source_arn  = "${aws_dynamodb_table.transactions.stream_arn}"
   function_name     = "${aws_lambda_function.totalnotifier.arn}"
   starting_position = "LATEST"
